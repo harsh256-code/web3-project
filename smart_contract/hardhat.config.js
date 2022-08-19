862ae23b4bc9b6f69ddb41d16361ff4e250964b3
HARSH VISHWAKARMA <harshvishwakarma111@gmail.com>
2022-08-19 14:04:40 +0530

    smart contract done

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,16 @@
-require("@nomicfoundation/hardhat-toolbox");
+// https://eth-goerli.g.alchemy.com/v2/ikk9R0kUIh84L5N23qkIp-7GTMYgJVXo
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const { defaultAccounts } = require("ethereum-waffle");
+
+require("@nomiclabs/hardhat-waffle");
 module.exports = {
-  solidity: "0.8.9",
+  solidity: "0.8.0",
+  networks: {
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/ikk9R0kUIh84L5N23qkIp-7GTMYgJVXo",
+      accounts: [
+        "fc7b11261ce5b20a42472897a5dc4ba00e11ec21e4393699d69ca720ddc24608",
+      ],
+    },
+  },
 };
