7c79921e61e6e9f5baa476a907fca98b63e59d40
HARSH VISHWAKARMA <harshvishwakarma111@gmail.com>
2022-08-19 19:27:02 +0530

    Trasaction fuction sucess backend done

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -19,11 +19,19 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 );
 
 const Welcome = () => {
-  const { connectWallet, currentAccount, formData, setFormData, handleChange } =
-    useContext(TransactionContext);
+  const {
+    connectWallet,
+    currentAccount,
+    formData,
+    sendTransaction,
+    handleChange,
+  } = useContext(TransactionContext);
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
     const { addressTo, amount, keyword, message } = formData;
+    e.preventDefault();
+    if (!addressTo || !amount || !keyword || !message) return;
+    sendTransaction();
   };
   return (
     <div className="flex w-full justify-center items-center">
